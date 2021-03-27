041e4bcbd3ecc4e33511955985486fa7093afd34
Junya-0220 <junya010220@gmail.com>
2021-03-27 23:02:06 +0900

    Noneとか

--- seigyokoubun_1.py
+++ seigyokoubun_1.py
@@ -25,3 +25,40 @@ if a > 0:
     print('a is positive')
     if b > 0:
         print('b is positive')
+
+y = [1, 2, 3]
+x = 1
+
+if 1 in y:
+    print('in')
+
+if 100 not in y:
+    print('not in')
+
+a = 1
+b = 2
+
+if a != b:
+    print('Not equal')
+
+is_ok = True
+if not is_ok:
+    print('hello')
+
+if is_ok:
+    print('ok')
+
+
+is_ok = ['a']
+
+
+if len(is_ok) > 0:
+    print('ok')
+else:
+    print('ng')
+
+is_empty = None
+print(is_empty)
+
+if is_empty is None:
+    print('None')
