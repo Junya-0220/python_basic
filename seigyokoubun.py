70018eb3b2c4b4098ff6ad23449e12e2c88dc21c
Junya-0220 <junya010220@gmail.com>
2021-03-27 22:50:50 +0900

    if

---
+++ seigyokoubun.py
@@ -0,0 +1,27 @@
+print('xxxxxx')
+# test
+print('yyyyyy')
+
+"""
+コメントを複数行書きたい場合は
+こんなかんじで囲ってあげるといいよ
+"""
+
+x = 10
+
+if x < 0:
+    print('negative')
+elif x == 0:
+    print('zero')
+elif x == 10:
+    print('1000000000')
+else:
+    print('positive')
+
+a = 5
+b = 10
+
+if a > 0:
+    print('a is positive')
+    if b > 0:
+        print('b is positive')
