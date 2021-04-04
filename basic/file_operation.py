2cc2ad9e2da34fd06f2971b41b258bde8e7e79d1
Junya-0220 <junya010220@gmail.com>
2021-04-04 08:39:10 +0000

    commit

--- basic/file_operation.py
+++ basic/file_operation.py
@@ -128,38 +128,38 @@
 # subprocess.run(['ls', '-al'])
 # subprocess.run('ls -al | grep test', shell=True)
 
-import shutil
-import os
-import time
-import datetime
+# import shutil
+# import os
+# import time
+# import datetime
 
-now = datetime.datetime.now()
-print(now)
-print(now.isoformat())
-print(now.strftime('%d/%m/%y-%H%M%S'))
+# now = datetime.datetime.now()
+# print(now)
+# print(now.isoformat())
+# print(now.strftime('%d/%m/%y-%H%M%S'))
 
-today = datetime.date.today()
-print(today)
-print(today.isoformat())
+# today = datetime.date.today()
+# print(today)
+# print(today.isoformat())
 
-t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
-print(t)
-print(t.strftime('%d/%m/%y-%H%M%S'))
+# t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+# print(t)
+# print(t.strftime('%d/%m/%y-%H%M%S'))
 
-print(now)
-d = datetime.timedelta(weeks=1)
-print(now + d)
+# print(now)
+# d = datetime.timedelta(weeks=1)
+# print(now + d)
 
-# print('###')
-# time.sleep(2)
-# print('###')
+# # print('###')
+# # time.sleep(2)
+# # print('###')
 
 
-file_name = 'test.txt'
+# file_name = 'test.txt'
 
-if os.path.exists(file_name):
-    shutil.copy(file_name, "{}.{}".format(
-        file_name, now.strftime('%Y_%m_%d_%H_%M_%S')))
+# if os.path.exists(file_name):
+#     shutil.copy(file_name, "{}.{}".format(
+#         file_name, now.strftime('%Y_%m_%d_%H_%M_%S')))
 
-with open(file_name, 'w') as f:
-    f.write('test')
+# with open(file_name, 'w') as f:
+#     f.write('test')
